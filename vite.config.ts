88f46ed3352cdf0b5308ec146caa217159201f6d
Ngọ Nhâm <nhamngooinfo@gmail.com>
2024-10-07 19:23:45 +0700

    update

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import path from 'path'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()] as any,
-  base: '/ShopeeClone/',
+  base: '/shopeeclone/',
   server: {
     port: 3000
   },
